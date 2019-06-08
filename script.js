0200213aef6228351fb1f6f604cdfe9c04bf6335
jtkfrancisco <43385696+jtkfrancisco@users.noreply.github.com>
2019-06-08 19:40:27 -0400

    Beta

---
+++ script.js
@@ -0,0 +1,8 @@
+//this is where the JS goes
+
+//Crown shyness img overlay and link handling 
+$("#IMG01-overlay").on('click', function() {
+	console.log("IMG01-overlay clicked")
+	window.open("https://en.wikipedia.org/wiki/Crown_shyness",'_blank')
+})
+
