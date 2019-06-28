56a64e5330d302faaa3fe31f8e31fdbf98d5ee62
jtkfrancisco <43385696+jtkfrancisco@users.noreply.github.com>
2019-06-28 14:26:55 -0400

    Beta Launch

---
+++ script.js
@@ -0,0 +1,53 @@
+$(".clickBox-home").on('click', function(){
+	window.location.href = "index.html";
+})
+
+$(".clickBox-work").on('click', function(){
+	window.location.href = "index.html";
+})
+
+$(".clickBox-contact").on('click', function(){
+	alert()
+})
+
+$(".clickBox-testimonials").on('click', function(){
+	alert()
+})
+
+$(".clickBox-services").on('click', function(){
+	alert()
+})
+
+$(".clickBox-logo").on('click', function(){
+	window.location.href = "logos.html";
+})
+
+$(".clickBox-poster").on('click', function(){
+	window.location.href = "posters.html";
+})
+
+$(".clickBox-video").on('click', function(){
+	window.location.href = "videos.html";
+})
+
+$(".clickBox-feature01").on('click', function(){
+	alert()
+})
+$(".clickBox-feature02").on('click', function(){
+	alert()
+})
+$(".clickBox-feature03").on('click', function(){
+	alert()
+})
+$(".clickBox-feature04").on('click', function(){
+	alert()
+})
+$(".clickBox-feature05").on('click', function(){
+	alert()
+})
+$(".clickBox-email").on('click', function(){
+	alert("I look forward to hearing from you \n please send me an email at \n jtkfrancisco@gmail.com")
+})
+$(".clickBox-instagram").on('click', function(){
+	window.open("https://www.instagram.com/jtkfrancisco")
+})
